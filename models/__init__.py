f2b099dfe4c8d8be17a925b13ccac784ac963b74
StephenAnthony8 <anthonysteve02@gmail.com>
2023-12-21 15:39:20 +0300

    changes

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-import os
 from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage 
 
-# Check the value of HBNB_TYPE_STORAGE environment variable
-storage_type = os.getenv('HBNB_TYPE_STORAGE', 'file')
 
-# Choose storage type based on the environment variable 
-if storage_type == 'db': 
-    storage = DBStorage()
-else:
-    storage = FileStorage()
-
+storage = FileStorage()
 storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -2,18 +2,53 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from models import storage
+
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    # question 6 block
+    id = Column(String(60), nullable=False, primary_key=True, default=str(uuid.uuid4()))
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    # endof q6 block
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        # new dict block
+        base_attributes = {
+            'id': str(uuid.uuid4()),
+            'created_at': datetime.now(),
+            'updated_at': datetime.now()
+            }
+
+        # end of dict block
         if not kwargs:
-            from models import storage
+            
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            # storage.new(self) moved according to question 6
+            self.save()
+        # new elif block
+        elif (set(base_attributes.keys()).issubset(kwargs.keys()) is False):
+            
+            if '__class__' in kwargs.keys():
+                del kwargs['__class__']
+
+            for attrib in base_attributes:
+                if attrib not in kwargs.keys():
+                    kwargs[attrib] = base_attributes[attrib]
+            self.__dict__.update(kwargs)
+            # storage.new(self) moved according to question 6
+            self.save()
+        # end of elif block
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -29,16 +64,27 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
+        
         self.updated_at = datetime.now()
+        storage.new(self) #moved from init(q6)
         storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
+        
+        # The below nonsense returns the classname
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
+        
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+
+        # delete the key _sa_instance_state if it exists
+        if ('_sa_instance_state' in dictionary.keys()):
+            del dictionary['_sa_instance_state']
         return dictionary
+    
+    def delete(self):
+        storage.delete(self)
