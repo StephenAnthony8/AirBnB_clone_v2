a4f71e228676f1dc6e83b4eeba63d7a7850affd8
GLetiwa <graceletiwa@gmail.com>
2023-12-23 20:10:11 +0000

    late

--- models/review.py
+++ models/review.py
@@ -9,8 +9,6 @@ class Review(BaseModel, Base):
 
     __tablename__ = 'reviews'
 
-    place_id = Column(String(60), ForeignKey(places.id),\
-            nullable=False)
-    user_id = Column(String(60),ForeignKey(users.id),\
-            nullable=False)
-    text = Column(String(1024), nullable=False)
+    place_id = Column(String(128), nullable=False)
+    user_id = Column(String(128), nullable=False)
+    text = Column(String(128), nullable=False)
