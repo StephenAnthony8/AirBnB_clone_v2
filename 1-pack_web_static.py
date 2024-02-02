3c58fb0006780aba4ba720d8d3505068f41516d1
StephenAnthony8 <anthonysteve02@gmail.com>
2024-02-02 04:45:22 +0300

    q1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" 1-pack_web_static - archives web_static directory & contents """
+from datetime import datetime
+from fabric.api import local, task
+import os
+
+
+@task
+def do_pack():
+    calendar_day, today = str(datetime.today()).split()
+    hour, minute, second = today.split(":")
+
+    label = "{}{}{}{}".format(calendar_day.replace("-", ""),
+                              hour,
+                              minute,
+                              second[: 2])
+    command1 = "mkdir -p versions"
+    command2 = f"tar -cvzf versions/web_static_{label}.tgz web_static"
+
+    if local(f"{command1} && {command2}").succeeded is True:
+        working_dir = f"{os.getcwd()}/versions/web_static_{label}.tgz"
+
+        return (working_dir)
+
+    return (None)
