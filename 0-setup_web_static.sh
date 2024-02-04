5f2083cac53f770d1a69e9bad7760bdbac748cf2
StephenAnthony8 <anthonysteve02@gmail.com>
2024-02-04 05:10:34 +0300

    q0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,15 +2,17 @@
 # sets up web servers for deployment of web_static
 
 # install nginx if not installed
-sudo apt update && sudo apt install -y nginx
+#sudo apt update && sudo apt install -y nginx
 
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 
+# echo files
 echo "Test content" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+echo "Hello World!" | sudo tee /var/www/html/index.html > /dev/null
 
 # create symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+rm -rf /data/web_static/current && sudo ln -s /data/web_static/releases/"test" /data/web_static/current
 
 # change ownership to ubuntu user & group
 sudo chown -R ubuntu:ubuntu /data/
@@ -20,22 +22,24 @@ sudo chown -R ubuntu:ubuntu /data/
 updated_config=$(cat <<eof
 
 server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
+	listen 80;
 
-        root /var/www/html;
+	root /var/www/html;
+	server_name www.sao-rct.tech;
 
-        add_header X-Served-By \$hostname;
+	add_header X-Served-By \$hostname;
 
-        location = /redirect_me {
-                return 301;
-        }
+	location = /redirect_me {
+	return 301;
+	}
 
-        location / {
-                try_files \$uri \$uri/ =404;
-        }
+	location / {
+
+	try_files \$uri \$uri/ =404;
+	}
+
 	location /hbnb_static {
-		alias /data/web_static/current/;
+	alias /data/web_static/current/;
 	}
 }
 eof
@@ -43,6 +47,5 @@ eof
 
 echo "$updated_config" | sudo tee /etc/nginx/sites-enabled/default >/dev/null
 
-sudo nginx -qt
-sudo nginx -s reload
+sudo nginx -qt && sudo service nginx start
 
---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" 2-do_deploy_web_static.py - distributes an archive to web servers"""
+
+import fabric
+
+c = Conn
