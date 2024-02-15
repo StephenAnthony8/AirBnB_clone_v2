a42f0aee7e7805929b358ccc9c7ad1da2d6ea383
StephenAnthony8 <anthonysteve02@gmail.com>
2024-02-15 16:05:49 +0300

    q0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""0-hello_route: starts flask web application"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    "hello: displays 'Hello HBNB' on /"
+    return ("Hello HBNB!")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
